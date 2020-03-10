48db59edc2a769da6a481eaa91d3ade19d8cd14c
Silpvapi <50416064+Silpvapi@users.noreply.github.com>
2020-03-10 09:31:49 -0400

    sample

---
+++ Test Suites/NewTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>80b2a683-5cd8-4288-aebe-5cb226457b4d</testSuiteGuid>
+   <testCaseLink>
+      <guid>7b3e3715-c703-477b-9997-2a6ea3a148fc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Folder/Complex_Order</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
